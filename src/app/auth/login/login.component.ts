1e0d5f81bcf832215142c322d6c143b4d1940060
vc-chirag <chirag.prajapati@viitor.cloud>
2023-06-24 15:17:55 +0530

    feat:Implement login page

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,13 +1,25 @@
+import { NgClass, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { FormsModule, NgForm } from '@angular/forms';
+import { ButtonComponent } from '@app/shared/utils/button/button.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [FormsModule, NgClass, NgIf, ButtonComponent],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  isSubmitted = false;
 
+  onSubmit(form: NgForm): void {
+    form.form.markAllAsTouched();
+    if (form.invalid) {
+      return;
+    }
+    this.isSubmitted = true;
+    console.log(form.value);
+
+  }
 }
