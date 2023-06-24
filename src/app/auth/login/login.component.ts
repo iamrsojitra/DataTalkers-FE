83872a9f49c6d8e0badfe9b13b50ebede214727b
vc-chirag <chirag.prajapati@viitor.cloud>
2023-06-24 20:57:26 +0530

    feat: implement api call for chatbot

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,10 @@
 import { NgClass, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { STORAGE } from '@app/core/constants/app.constant';
+import { AuthenticationService } from '@app/core/services/authentication.service';
+import { StorageService } from '@app/core/services/storage.service';
 import { ButtonComponent } from '@app/shared/utils/button/button.component';
 import { InputWhitespaceDirective } from '@directives/prevent-whitespace.directive';
 import { ToastService } from '@services/toast-service';
@@ -15,16 +19,39 @@ import { ToastService } from '@services/toast-service';
 export class LoginComponent {
   isSubmitted = false;
   constructor(
-    private toasterService: ToastService
+    private toasterService: ToastService,
+    private autheticationService: AuthenticationService,
+    private storageService: StorageService,
+    private router: Router
   ) { }
 
   onSubmit(form: NgForm): void {
-    this.toasterService.show("hello", { delay: 1000 });
     form.form.markAllAsTouched();
     if (form.invalid) {
       return;
     }
     this.isSubmitted = true;
+    const params = {
+      ...form.value,
+      port: 3306
+    }
+    this.autheticationService.dbConnection(params).subscribe({
+      next: (res) => {
+        if (res) {
+          this.isSubmitted = false;
+          this.storageService.set(STORAGE.TOKEN, res.accessToken);
+          this.router.navigate(['/']).then(() => {
+            this.toasterService.show("Database connection established", { classname: 'bg-success text-light', delay: 2000 });
+          });
+        }
+      },
+      error: (err) => {
+        if (err) {
+          this.toasterService.show("There is some error while connect to Database", { classname: 'bg-danger text-light', delay: 1000 })
+          this.isSubmitted = false;
+        }
+      }
+    })
     console.log(form.value);
 
   }
