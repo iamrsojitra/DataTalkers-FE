c341e2d7086f20e1a9bc4c90128ed3d7ed7e56e8
Rakesh Sojitra <rakesh.sojitra@viitor.cloud>
2023-06-25 08:21:48 +0530

    fix: web related changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
   PRODUCTION: false,
-  BASE_URL: 'http://localhost:4200',
+  BASE_URL: 'http://65.2.183.216/web/',
   SERVER_URL: 'http://65.2.183.216/',
-  FRONTEND_URL: 'https://datatalkers.netlify.app/auth/login'
 };
